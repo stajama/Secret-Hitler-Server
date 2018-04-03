fea995caa4d721497b31294bd6cc13a3d2f2951c
stajama <stajama@yahoo.com>
2018-04-03 14:41:34 -0500

    Update getStatus() helper.

--- shs/settings.py
+++ shs/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'shs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "DATABASE",
+        'NAME': "default",
         'HOST': 'localhost',
         'PORT': '',
     }
