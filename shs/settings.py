be4bac3a70fdb44fae6e240b014cb761df26d02e
stajama <stajama@yahoo.com>
2018-04-03 14:38:47 -0500

    Update getStatus() helper.

--- shs/settings.py
+++ shs/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -26,7 +29,7 @@ SECRET_KEY = '971*8v)6xqsew@hgay!lqt3mxoa3w)zoo+1hws+jnr+acn#s%i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secret-ravine-44641.herokuapp.com', 'herokuapp.com']
 
 
 # Application definition
@@ -77,8 +80,10 @@ WSGI_APPLICATION = 'shs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "DATABASE",
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
